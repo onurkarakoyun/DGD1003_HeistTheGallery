4738d0326ccca21179a57dc113e268f29a78adb0
ygtshn024 <ygtshn024@gmail.com>
2025-12-02 15:56:46 +0300

    lazer

--- Assets/Scripts/LazerTrap.cs
+++ Assets/Scripts/LazerTrap.cs
@@ -1,16 +1,13 @@
-using UnityEngine;
+ using UnityEngine;
 
-public class lazer : MonoBehaviour
+public class LaserTrap : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Oyuncu lazere yakalandı!");
+            // Buraya oyuncuyu öldürme, yeniden başlatma vs. ekleyebilirsin
+        }
     }
 }
